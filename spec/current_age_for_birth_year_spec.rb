ed633a47fb2330ecc8369218cfdaf72c0effa5cf
Adam Giegel <adam.giegel@gmail.com>
2018-10-04 16:39:59 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -8,4 +8,4 @@ describe "current_age_for_birth_year method" do
   end
 end
 
-beg current_age_for_birth_year(birth_year) 2003 - birth_year end
+ current_age_for_birth_year(birth_year) 2003 - birth_year end
